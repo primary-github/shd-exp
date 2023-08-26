35537d7fbe8710c945029ca79bea813104370788
joshuamURD <jmacmaster9@gmail.com>
2023-08-26 15:24:36 +0800

    Updates

---
+++ content/documentation.md
@@ -0,0 +1,7 @@
+# Zettlekasten
+Zettlekasten is a phrase that is getting more and more popular. It describes a form of note-taking that was popularized by Nickolas Luuhman who was a prolific writer of his time. Luuhman attributed all of his amassed publications to his workflow which he termed the "zettlekasten" (german for slipbox). The principles of the zettlekasten were originally designed under Luuhman's system using index cards as notes. The Shared Experience
+
+# Concepts and principles
+- [[note taking must be personal]]
+- [[connecting new ideas to what you already know strengthens your memory of new ideas]]
+- 
---
+++ quartz/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { QuartzComponentConstructor } from "./types"
+import style from "./styles/footer.scss"
+import { version } from "../../package.json"
+
+interface Options {
+  links: Record<string, string>
+}
+
+export default ((opts?: Options) => {
+  function Footer() {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
+    return (
+      <footer>
+        <hr />
+        <p>
+          The Shared Experience
+        </p>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    )
+  }
+
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor

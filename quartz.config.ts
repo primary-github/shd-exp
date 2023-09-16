835de65382115223c012fec7c5fd142f9b0b56d1
sam <116995682+primary-github@users.noreply.github.com>
2023-09-16 12:02:46 +0800

    vault backup: 2023-09-16 12:02:46

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Shared Experience 🕯",
+    pageTitle: "Shared Experience 🕯",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
